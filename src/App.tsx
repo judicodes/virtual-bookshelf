c04ef68f124a76b2b71f301d129c49a3ed79434b
Judith Böhlert <hello@judithboehlert.com>
2024-04-24 15:39:04 +0200

    Display books.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BookItem } from "./components/Book";
+import { getAllBooks } from "./services/books.service";
+
 function App() {
+  const books = getAllBooks();
+
   return (
-    <div className="flex flex-row justify-center mt-8">
-      <h1 className="font-bold text-3xl">
-        This shall be our Virtual Bookshelf
-      </h1>
+    <div className="m-4 grid gap-4">
+      <h1 className="font-bold text-3xl">All books</h1>
+      <ul className="grid gap-2 grid-cols-[repeat(auto-fit,minmax(22ch,1fr))]">
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </ul>
+
+      <button>Add new book</button>
     </div>
   );
 }
---
+++ src/components/Book.tsx
@@ -0,0 +1,24 @@
+import { Book } from "@/services/books.service";
+
+export function BookItem({
+  title,
+  author,
+  publicationYear,
+  description,
+  rating
+}: Book) {
+  return (
+    <li className="grid gap-1 p-4 content-start rounded-lg border border-slate-300">
+      <p className="font-bold text-xl">{title}</p>
+      {author && <p>{author}</p>}
+      {publicationYear && <time>{publicationYear}</time>}
+      {description && <p>{description}</p>}
+      {rating && (
+        // TODO: Make this clickable star icons later on
+        <p>
+          Rating: <span>{rating}</span>
+        </p>
+      )}
+    </li>
+  );
+}
