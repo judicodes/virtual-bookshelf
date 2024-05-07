48997fd4e8a8478ac0f2ac8f9eb5ffc89406273b
Judith Böhlert <hello@judithboehlert.com>
2024-05-07 08:59:45 +0200

    Set up Supabase client.

---
+++ src/services/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export default supabase;
